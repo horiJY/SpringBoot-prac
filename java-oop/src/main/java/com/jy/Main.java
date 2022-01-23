683de274276db85c8bae6e26e03a4a7e2fbc4597
hori_q@naver.com <hori_q@naver.com>
2022-01-24 01:04:45 +0900

    Update #3 - main
    구현 클래스를 인터페이스로 변경
    #3은 여기서 마무리.

--- java-oop/src/main/java/com/jy/Main.java
+++ java-oop/src/main/java/com/jy/Main.java
@@ -1,12 +1,13 @@
 package com.jy;
 
-import com.jy.logic.BubbleSort;
+import com.jy.logic.JavaSort;
+import com.jy.logic.Sort;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        BubbleSort<String> sort = new BubbleSort<>();
+        Sort<String> sort = new JavaSort<>();
         System.out.println("result] " + sort.sort(Arrays.asList(args)));
     }
 }
